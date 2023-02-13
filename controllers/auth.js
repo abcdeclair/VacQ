e1ebec077cadc9be48a5f94169cc1546bf4467bb
abcdeclair <abcdeclair@gmail.com>
2023-02-13 18:50:22 +0700

    update authorization

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,9 +17,10 @@ exports.register = async (req, res, next) => {
 
     //Create token
 
-    const token = user.getSignedJwtToken();
+    //const token = user.getSignedJwtToken();
 
-    res.status(200).json({ success: true, token });
+    //res.status(200).json({ success: true, token });
+    sendTokenResponse(user, 200, res);
   } catch (err) {
     res.status(400).json({ success: false });
     console.log(err.stack);
@@ -54,7 +55,41 @@ exports.login = async (req, res, next) => {
   }
 
   //Create token
+  //const token = user.getSignedJwtToken();
+
+  //res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
+};
+
+//Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  //Create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
+
+//@desc     Get current Logged in user
+//@route    POST /api/v1/auth/me
+//@access   Private
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 //Load env vars
@@ -13,11 +14,14 @@ const app = express();
 //add body parser
 app.use(express.json());
 
+//Cookie parser
+app.unsubscribe(cookieParser());
+
 const hospitals = require("./routes/hospitals");
-const auth = require('./routes/auth');
+const auth = require("./routes/auth");
 
 app.use("/api/v1/hospitals", hospitals);
-app.use('/api/v1/auth', auth);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
